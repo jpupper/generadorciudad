5d7f952d2865a120f3df3c07d603ed8c3a68506a
Jpupper <julian.d.puppo@gmail.com>
2025-10-30 17:34:12 -0300

    cambios de socket

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 // Config mínimo adaptado al generador de ciudades 3D
 const config = {
   isLocal: ['localhost', '127.0.0.1'].includes(window.location.hostname),
-  socketPath: '/generadorciudades/socket.io',
+  socketPath: '/socket.io',
   gridSize: 1,
   world: {
     groundSize: 200
